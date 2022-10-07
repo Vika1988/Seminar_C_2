edf1ba6da1fecb3c1a5adad527e2d3cdaef3a9b1
*V* <cnevzorov@yandex.ru>
2022-10-07 15:42:16 +0300

    Программа проверяет, является ли одно любое число квалратом другого

---
+++ Task_5/Program.cs
@@ -0,0 +1,11 @@
+﻿/*Напишите программу, которая на вход принимает 2 числа и проверяет является ли одно число квадратом другого*/
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+if(a / b == b || b / a == a){
+    Console.WriteLine("Одно число является квадратом другого");
+}
+else{
+    Console.WriteLine("Ни одно из чисел не является квадратом другого");
+}
